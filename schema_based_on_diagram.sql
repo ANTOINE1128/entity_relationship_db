11dd1bab38c41b765121cabfcccce5e5e18502a6
kessie <kayp3477@gmail.com>
2023-07-25 15:32:15 +0000

    Add indexes for medical history and treatment IDs in medical_histories_treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,16 +3,16 @@ Create TABLE patients(
     name VARCHAR(255),
     date_of_birth DATE,
     patient_id INTEGER,
-    
+
 );
 
 CREATE TABLE medical_histories(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     addmited_at TIMESTAMP(6),
     patient_id INT,
-    PRIMARY KEY(patient_id), 
-    CONSTRAINT fk_patient, 
-    FOREIGN KEY(patient_id) 
+    PRIMARY KEY(patient_id),
+    CONSTRAINT fk_patient,
+    FOREIGN KEY(patient_id)
     REFERENCES patients(patient_id),
     status VARCHAR(50)
     ON DELETE SET NULL,
@@ -25,9 +25,9 @@ CREATE TABLE invoices(
     generated_at TIMESTAMP(6),
     payed_at TIMESTAMP(6),
     medical_historie_id INTEGER,
-    PRIMARY KEY(medical_history_id), 
-    CONSTRAINT fk_medical_history, 
-    FOREIGN KEY(medical_history_id) 
+    PRIMARY KEY(medical_history_id),
+    CONSTRAINT fk_medical_history,
+    FOREIGN KEY(medical_history_id)
     REFERENCES medical_histories(medical_history_id)
     ON DELETE SET NULL,
     invoice_id INTEGER,
@@ -39,15 +39,15 @@ CREATE TABLE invoices_items (
     quantity INTEGER,
     total_price DECIMAL(10,2),
     invoice_id INTEGER,
-    PRIMARY KEY(invoice_id), 
-    CONSTRAINT fk_invoice, 
-    FOREIGN KEY(invoice_id) 
+    PRIMARY KEY(invoice_id),
+    CONSTRAINT fk_invoice,
+    FOREIGN KEY(invoice_id)
     REFERENCES invoices(invoice_id)
     ON DELETE SET NULL,
     treatment_id INTEGER,
-    PRIMARY KEY(treatment_id), 
-    CONSTRAINT fk_invoice, 
-    FOREIGN KEY(treatment_id) 
+    PRIMARY KEY(treatment_id),
+    CONSTRAINT fk_invoice,
+    FOREIGN KEY(treatment_id)
     REFERENCES treatments(treatment_id)
      ON DELETE SET NULL
 );
@@ -65,7 +65,10 @@ CREATE TABLE medical_histories_treatments (
     medical_history_id INTEGER,
     treatment_id INTEGER,
     PRIMARY KEY (medical_history_id, treatment_id),
-    FOREIGN KEY (treatment_id) REFERENCES treatments(treatment_id) 
-    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(medical_history_id) 
+    FOREIGN KEY (treatment_id) REFERENCES treatments(treatment_id)
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(medical_history_id)
     ON DELETE SET NULL
 )
+
+CREATE INDEX idx_medical_history_id ON medical_histories_treatments(medical_history_id);
+CREATE INDEX idx_treatment_id ON medical_histories_treatments (treatment_id);
